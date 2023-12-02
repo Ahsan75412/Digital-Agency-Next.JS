71b71e9e51f2eb36bc65978d851848efbc109d86
Ahsan <ahsanpub16b@gmail.com>
2023-12-02 14:50:55 +0600

    Navbar Added here!

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,47 +1,39 @@
 "use client"
-
+import Link from "next/link";
 
 
 const NavBar = () => {
+
+    const navOptions = <>
+        <li><Link href="/home"> Home </Link></li>
+        <li><Link href="/team"> Team </Link></li>
+        <li><Link href="/service"> Service </Link></li>
+        <li><Link href="/projects"> Projects </Link></li>
+        <li><Link href="/testimonials"> Testimonials </Link></li>
+        <li className="btn btn-outline btn-sm"><Link href="/login">Login</Link></li>
+        <li className="btn btn-sm ml-3 bg-[#20B15A] text-white"><Link href="/register">Register</Link></li>
+    
+    </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-[#D7F5DC] max-w-screen-xl mx-auto">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-semibold">
+                        {navOptions}
+                    
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="font-bold text-xl">Digital<span className="text-[#F55F1D]">Agency</span></a>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+            <div className="navbar-center hidden md:flex">
+                <ul className="menu menu-horizontal px-1 font-semibold">
+                    {navOptions}
                 </ul>
             </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
-            </div>
+         
         </div>
     );
 };
