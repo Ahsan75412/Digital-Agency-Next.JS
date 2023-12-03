8b130f18446d0a8a0017479e136cb614cff334a7
Ahsan <ahsanpub16b@gmail.com>
2023-12-04 03:15:27 +0600

    some file modified

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,6 @@
 import BrandList from "@/components/BrandList";
 import FeaturedProjects from "@/components/FeaturedProjects";
 import Hero from "@/components/Hero";
-import NavBar from "@/components/NavBar";
 import Stats from "@/components/Stats";
 import SubscribeSection from "@/components/SubscribeSection";
 import WorkList from "@/components/WorkList";
